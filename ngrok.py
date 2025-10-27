e0c7f1d930720ba1c86bbf92388b7718c244a286
Fateme Ebrahimi <82450336+ftmebrahimi@users.noreply.github.com>
2025-10-27 21:14:32 +0330

    Update ngrok.py

--- ngrok.py
+++ ngrok.py
@@ -1,10 +1,10 @@
 from pyngrok import ngrok
 import signal
-ngrok.set_auth_token("31PIVUUImUOcfEfnvDg7VtTlDmb_4KSxGwbjT138krGZReDNB")
+ngrok.set_auth_token("your ngrok api token")
 
 listener = ngrok.connect(
     addr="localhost:3000",
-    metadata="example listener metadata from python",
+    metadata="avatar agent public url",
 )
 print(f"Please click on the text below {listener}")
 print("Tunnel is running. Press Ctrl+C to stop.")
