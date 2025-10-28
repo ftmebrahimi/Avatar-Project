cc06cbef49b728699612d9f4d05b3800b6a2243c
Fateme Ebrahimi <82450336+ftmebrahimi@users.noreply.github.com>
2025-10-28 18:40:33 +0330

    pipeline of avatar agent contains ASR-LLM-TTS-Avatar

--- avatar_agent.py
+++ avatar_agent.py
@@ -58,6 +58,5 @@ async def entrypoint(ctx: JobContext):
         room=ctx.room,
     )
 
-
 if __name__ == "__main__":
     cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint, prewarm_fnc=prewarm))
